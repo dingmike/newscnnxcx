d2d8d08b91ea560842955cfa602edacdd29419a6
slateding <mikezhangding@gmail.com>
2018-05-25 11:57:31 +0800

    api

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,7 +3,7 @@ const api = require('../../config/api.js');
 const user = require('../../services/user.js');
 
 //获取应用实例
-const app = getApp()
+const app = getApp();
 Page({
   data: {
     newGoods: [],
