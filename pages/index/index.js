d93734814a884b3ed8a8b7e13a88b1f11e7bb712
slateding <mikezhangding@gmail.com>
2018-05-25 18:14:18 +0800

    newscnn

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,52 +5,56 @@ const user = require('../../services/user.js');
 //获取应用实例
 const app = getApp();
 Page({
-  data: {
-    newGoods: [],
-    hotGoods: [],
-    topics: [],
-    brands: [],
-    floorGoods: [],
-    banner: [],
-    channel: []
-  },
-  onShareAppMessage: function () {
-    return {
-      title: 'newscnn',
-      desc: 'newscnn fans shop',
-      path: '/pages/index/index'
-    }
-  },
+    data: {
+        newGoods: [],
+        hotGoods: [],
+        topics: [],
+        brands: [],
+        floorGoods: [],
+        banner: [],
+        channel: []
+    },
+    onPullDownRefresh() {
+        // wx.showNavigationBarLoading(); //在标题栏中显示加载
+        this.getIndexData();
+    },
+    onShareAppMessage: function () {
+        return {
+            title: 'newscnn',
+            desc: 'newscnn fans shop',
+            path: '/pages/index/index'
+        }
+    },
 
-  getIndexData: function () {
-    let that = this;
-    util.request(api.IndexUrl).then(function (res) {
-      if (res.errno === 0) {
-        that.setData({
-          newGoods: res.data.newGoodsList,
-          hotGoods: res.data.hotGoodsList,
-          topics: res.data.topicList,
-          brand: res.data.brandList,
-          floorGoods: res.data.categoryList,
-          banner: res.data.banner,
-          channel: res.data.channel
+    getIndexData: function () {
+        let that = this;
+        util.request(api.IndexUrl).then(function (res) {
+            if (res.errno === 0) {
+                that.setData({
+                    newGoods: res.data.newGoodsList,
+                    hotGoods: res.data.hotGoodsList,
+                    topics: res.data.topicList,
+                    brand: res.data.brandList,
+                    floorGoods: res.data.categoryList,
+                    banner: res.data.banner,
+                    channel: res.data.channel
+                });
+            }
         });
-      }
-    });
-  },
-  onLoad: function (options) {
-    this.getIndexData();
-  },
-  onReady: function () {
-    // 页面渲染完成
-  },
-  onShow: function () {
-    // 页面显示
-  },
-  onHide: function () {
-    // 页面隐藏
-  },
-  onUnload: function () {
-    // 页面关闭
-  },
+    },
+    onLoad: function (options) {
+        this.getIndexData();
+    },
+    onReady: function () {
+        // 页面渲染完成
+    },
+    onShow: function () {
+        // 页面显示
+    },
+    onHide: function () {
+        // 页面隐藏
+    },
+    onUnload: function () {
+        // 页面关闭
+    },
 })
